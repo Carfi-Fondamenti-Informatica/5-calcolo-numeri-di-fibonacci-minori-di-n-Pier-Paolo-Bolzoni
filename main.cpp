443608a0d34c789ad9906c9218405e80db6a26c7
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2022-04-03 18:48:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
-#include <iostream>
 using namespace std;
 
 int main() {
 
-    int n=0, a=0, b=1, c=0,i ;
+    int n=0, a=1, b=0, c=0,i ;
 
     cin >> n ;
 
-        for ( i = 0 ; i < n ; i++ ){
-                c = a + b;
-                b = a;
-                a = c;
+        while(n >= c){
+            c= a + b;
+            a = b;
+            b = c;
             if(n > c ) {
                 cout << c << endl;
-            }else{
-                break;
             }
        }
 
